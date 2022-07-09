b4d9509125cdb111b0082aa7e8a5ea03fbc6b518
Yash Prakash <yashprakash13@gmail.com>
2022-07-09 09:34:29 -0400

    docker compose with postgres service working.

--- padfoot_src/padfoot_src/settings.py
+++ padfoot_src/padfoot_src/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'padfoot_src.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
